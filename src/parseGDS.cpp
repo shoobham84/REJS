10db040a0d8625bfc89f81403f9f936ca650b091
Shubham Nayak <shubhamnayak392reborn@gmail.com>
2026-08-23 20:07:20 +0530

    edit parseGDS

--- src/parseGDS.cpp
+++ src/parseGDS.cpp
@@ -4,47 +4,27 @@
 #include <string_view>
 #include <ranges>
 #include <algorithm>
+#include "json.hpp"
 
-struct Layer {
-    uint32_t layer;
-    uint32_t dataType;
-    std::string_view name;
+using Json = nlohmann::json;
+
+struct PinInfo
+{
+    std::string_view Name;
+    uint32_t Layer;
+    double x, y;
 };
 
-// straigt aaway pastin ts
-static constexpr Layer ROUTING_LAYERS[] = {
-    {67, 20, "li1"},
-    {67, 44, "mcon"},
-    {68, 20, "met1"},
-    {68, 44, "via"},
-    {69, 20, "met2"},
-    {69, 44, "via2"},
-    {70, 20, "met3"},
-    {70, 44, "via3"},
-    {71, 20, "met4"},
-    {71, 44, "via4"},
-    {72, 20, "met5"},
+struct Instance 
+{
+    int id;
+    std::string_view cellType;
+    double x;
+    double y;
+    double Rotation;
+    bool xReflected;
 };
 
-constexpr int ROUTING_LAYER_COUNT { 11 };
-
-constexpr bool isRoutingTag(gdstk::Tag tag) noexcept {
-    const uint32_t layer { gdstk::get_layer(tag) };
-    const uint32_t type { gdstk::get_type(tag) };
-    
-    for (int i{0}; i < ROUTING_LAYER_COUNT; ++i) {
-        if( ROUTING_LAYERS[i].layer == layer && ROUTING_LAYERS[i].dataType == type) {return true;}
-    }
-    return false;
-}
-
-constexpr std::string_view layerNameFor(uint32_t layer, uint32_t datatype) {
-    for (const auto& [lr, dt, name] : ROUTING_LAYERS) {
-        if (lr == layer && dt == datatype) return name; 
-    }
-    return "not found -:ERR";
-
-}
 
 constexpr bool isFiller(std::string_view Name) noexcept {
     if (!Name.starts_with("sky130_fd_sc_hd__")) return true;
@@ -53,14 +33,6 @@ constexpr bool isFiller(std::string_view Name) noexcept {
     return std::ranges::any_of(filters, [&](auto f) { return Name.find(f) != std::string_view::npos; });
 }
 
-struct CellRec {
-};
-
-struct PolygonRecord {
-};
-
-
-void decompRectinilinear() {} // rectilinear polygon -> rectangle, get with ai idk
 
 int main() {
     std::println("parseGDS compiled successfully with GDSTK!");
