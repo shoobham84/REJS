7baa2d4cd620cba2e3376e475b69752d28acb802
Shubham Nayak <shubhamnayak392reborn@gmail.com>
2026-08-27 22:03:14 +0530

    topological sorting of combinational feedback

--- src/solveSAT.py
+++ src/solveSAT.py
@@ -1,4 +1,5 @@
 import re
+from collections import deque
 
 def parse_netlist(path="outputs/extracted_netlist.v"):
     with open(path) as f:
@@ -36,3 +37,49 @@ def parse_netlist(path="outputs/extracted_netlist.v"):
     print(f"Parsed {len(logicGates)} combinational gates, {len(flipFlops)} dflipflops, {len(pad)} pad bindings")
 
     return logicGates, flipFlops, pad
+
+
+def topologicalSort(logic_gates):
+    net_driver = {}
+    gate_ins = {}
+    for i, (inst, cell, ports) in enumerate(logic_gates):
+        out = None
+        ins = []
+        for pin, net in ports.items():
+            if pin in ("X", "Y", "HI", "LO"):
+                if out is None:
+                    out = net
+            elif net and net not in ("1'b0", "1'b1"):
+                ins.append(net)
+        if out:
+            net_driver[out] = i
+        gate_ins[i] = ins
+
+    n = len(logic_gates)
+    in_deg = [0] * n
+    deps = [[] for _ in range(n)]
+    for i, ins in gate_ins.items():
+        for net in ins:
+            if net in net_driver:
+                j = net_driver[net]
+                if j != i:
+                    deps[j].append(i)
+                    in_deg[i] += 1
+
+    q = deque(i for i in range(n) if in_deg[i] == 0)
+    order = []
+    while q:
+        c = q.popleft()
+        order.append(c)
+        for d in deps[c]:
+            in_deg[d] -= 1
+            if in_deg[d] == 0:
+                q.append(d)
+
+    visited = set(order)
+    for i in range(n):
+        if i not in visited:
+            order.append(i)
+
+    return [logic_gates[i] for i in order]
+
